f66a0a997943042c199c1ff568b3e2031053b21e
Sławomir Marczyński <slawek@zut.edu.pl>
2020-05-31 15:24:23 +0200

    Dodany szablon z copyright'em
    Signed-off-by: Sławomir Marczyński <slawek@zut.edu.pl>

---
+++ template.m
@@ -0,0 +1,15 @@
+% Przedmiot: Techniki Obliczeniowe 
+% Kierunek studiów: Mechatronika 
+% Semestr: 2
+% Rok akademicki: 2019/2020
+% Data (dzień-miesiąc-rok): <<31-12-2020>>
+%
+% Imię:             <<imię>>
+% Nazwisko:         <<nazwisko>>
+% Numer albumu ZUT: <<numer albumu>>
+%
+% Nazwa programu:   <<nazwa programu>>
+%
+% 2020 copyright <<imię i nazwisko (raz jeszcze)>>
+%
+% Tekst poniżej jest objęty licencją CC-BY-NC-SA.
